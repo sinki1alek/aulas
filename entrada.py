fab1df9e7e91cbe04e056ee2271b7becf0d531b0
Your Name <you@example.com>
2026-02-19 10:58:11 -0300

    final aula de string

--- entrada.py
+++ entrada.py
@@ -16,3 +16,18 @@ PALAVRA = 'JACA'
 #colocar a string como toda minuscula
 print('colocando o texto em minuscula: ', PALAVRA.lower())
 
+#contar caracter da string
+palavra_contar = 'banana'
+print('contar a letra b', palavra_contar.count('b') )
+print('contar a letra a', palavra_contar.count('a') )
+print('contar a letra n', palavra_contar.count('n') )
+
+print(email)
+a_c = email.count('a')
+e_c = email.count('e')
+i_c = email.count('i')
+o_c = email.count('o')
+u_c = email.count('u')
+nova_senha = 'a' + str(a_c) + 'e'+ str(e_c) + 'i'+ str(i_c) + 'o'+ str(o_c) + 'u'+ str(u_c)
+print(nova_senha)
+
